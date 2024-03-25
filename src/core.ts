4792d54d049c4a2c0640781e4eed109574ffdbdc
DenisKozhevnikov <denis.bugagashkin@yandex.ru>
2024-03-25 17:12:39 +0100

    fix: funsdkcoremodule changed format file. merged main

--- src/core.ts
+++ src/core.ts
@@ -11,58 +11,58 @@ export function createCalendarEvent(
   return funsdk.createCalendarEvent(a, b, cb);
 }
 
-// export enum CUSTOM_PWD_TYPE {
-//   E_PWD_GENERAL, // Default, universal encryption (MD5 encryption)
-//   E_PWD_QUNGUANG, // is a special case before compatibility. Prefix is "QunGuang_". Prefix is added before encryption
-//   E_PWD_PREFIX_BEFORE_ENCRYPT, // Prefix is added before encryption
-//   E_PWD_PREFIX_BACK_ENCRYPT, // Add the prefix after encryption
-//   E_PWD_GIGA, // giga is a special case before compatibility. After encryption, prefix is added. json parameter szEncType is passed to the device: "GIGA" (other default values are "MD5").
-//   E_PWD_CUSTOM_MAX,
-// }
+export enum CUSTOM_PWD_TYPE {
+  E_PWD_GENERAL, // Default, universal encryption (MD5 encryption)
+  E_PWD_QUNGUANG, // is a special case before compatibility. Prefix is "QunGuang_". Prefix is added before encryption
+  E_PWD_PREFIX_BEFORE_ENCRYPT, // Prefix is added before encryption
+  E_PWD_PREFIX_BACK_ENCRYPT, // Add the prefix after encryption
+  E_PWD_GIGA, // giga is a special case before compatibility. After encryption, prefix is added. json parameter szEncType is passed to the device: "GIGA" (other default values are "MD5").
+  E_PWD_CUSTOM_MAX,
+}
 
-// export type FunSDKInitParams = {
-//   customPwdType?: CUSTOM_PWD_TYPE | number; // 0
-//   customPwd?: string;
-//   customServerAddr?: string;
-//   customPort?: number;
-// };
+export type FunSDKInitParams = {
+  customPwdType?: CUSTOM_PWD_TYPE | number; // 0
+  customPwd?: string;
+  customServerAddr?: string;
+  customPort?: number;
+};
 
-// // export function funSDKInit(params: FunSDKInitParams): Promise<any> {
-// export function funSDKInit(params: FunSDKInitParams = {}): Promise<any> {
-//   return funsdk.init(params);
-// }
+// export function funSDKInit(params: FunSDKInitParams): Promise<any> {
+export function funSDKInit(params: FunSDKInitParams = {}): Promise<any> {
+  return funsdk.init(params);
+}
 
-// export type FunSDKSysSetServerIPPortParams = {
-//   serverKey:
-//     | 'APP_SERVER'
-//     | 'STATUS_P2P_SERVER'
-//     | 'STATUS_DSS_SERVER'
-//     | 'STATUS_RPS_SERVER'
-//     | 'STATUS_IDR_SERVER'
-//     | 'HLS_DSS_SERVER'
-//     | 'CONFIG_SERVER'
-//     | 'UPGRADE_SERVER'
-//     | 'CAPS_SERVER'
-//     | string;
-//   serverIpOrDomain: string;
-//   serverPort: number;
-// };
+export type FunSDKSysSetServerIPPortParams = {
+  serverKey:
+    | 'APP_SERVER'
+    | 'STATUS_P2P_SERVER'
+    | 'STATUS_DSS_SERVER'
+    | 'STATUS_RPS_SERVER'
+    | 'STATUS_IDR_SERVER'
+    | 'HLS_DSS_SERVER'
+    | 'CONFIG_SERVER'
+    | 'UPGRADE_SERVER'
+    | 'CAPS_SERVER'
+    | string;
+  serverIpOrDomain: string;
+  serverPort: number;
+};
 
-// /**
-//  * Use after initialization
-//  *
-//  * server keys: 'APP_SERVER'
-//     , 'STATUS_P2P_SERVER'
-//     , 'STATUS_DSS_SERVER'
-//     , 'STATUS_RPS_SERVER'
-//     , 'STATUS_IDR_SERVER'
-//     , 'HLS_DSS_SERVER'
-//     , 'CONFIG_SERVER'
-//     , 'UPGRADE_SERVER'
-//     , 'CAPS_SERVER'
-//  */
-// export function funSDKSysSetServerIPPort(
-//   params: FunSDKSysSetServerIPPortParams
-// ) {
-//   funsdk.SysSetServerIPPort(params);
-// }
+/**
+ * Use after initialization
+ *
+ * server keys: 'APP_SERVER'
+    , 'STATUS_P2P_SERVER'
+    , 'STATUS_DSS_SERVER'
+    , 'STATUS_RPS_SERVER'
+    , 'STATUS_IDR_SERVER'
+    , 'HLS_DSS_SERVER'
+    , 'CONFIG_SERVER'
+    , 'UPGRADE_SERVER'
+    , 'CAPS_SERVER'
+ */
+export function funSDKSysSetServerIPPort(
+  params: FunSDKSysSetServerIPPortParams
+) {
+  funsdk.SysSetServerIPPort(params);
+}
